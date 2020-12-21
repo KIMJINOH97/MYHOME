995127cb65eec76e1c5be9bebb085ee1d5c1a418
KIMJINOH97 <rlawlsdh1127@naver.com>
2020-12-22 03:18:32 +0900

    feat: google map api 이용해 구현

---
+++ src/screen/MapScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import Map from '../components/Map';
+
+const MapScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Map />
+    </View>
+  );
+};
+
+export default MapScreen;
