f7ad17f77945a137a258fe5b4191c7e44a5d90ff
KIMJINOH97 <rlawlsdh1127@naver.com>
2020-12-21 18:37:25 +0900

    feat: 네비게이션 바 추가

--- src/components/navigations/MyHomeStack.js
+++ src/components/navigations/MyHomeStack.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-
+//   StackViewTransitionConfigs
 import HomeScreen from '../../screen/HomeScreen.js';
 import AboutScreen from '../../screen/AboutScreen.js';
+import MyHomeTab from './MyHomeTab.js';
 
+// const Navigation = createStackNavigator(
+//   { screenA: ComponentA, screenB: ComponentB },
+//   {
+//     mode: 'card',
+//     transitionConfig: () => StackViewTransitionConfigs.SlideFromRightIOS,
+//   }
+// );
 const Stack = createStackNavigator();
 
 const MyHomeStack = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="MyHomeTa" headerMode="screen">
+        <Stack.Screen
+          name="MyHomeTa"
+          component={MyHomeTab}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
         <Stack.Screen name="About" component={AboutScreen}></Stack.Screen>
       </Stack.Navigator>
