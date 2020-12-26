1ebebb7afee1994f2cc331ebb8c3948a7bae3a88
KIMJINOH97 <rlawlsdh1127@naver.com>
2020-12-27 00:19:54 +0900

    chore: change dummy data

--- src/screen/HomeListScreen.js
+++ src/screen/HomeListScreen.js
@@ -9,25 +9,28 @@ const DummyList = [
     id: '1',
     title: '신촌스테이하이',
     type: '월세',
-    money: 100,
-    bojoung: 100,
+    money: 45,
+    bojoung: 1000,
     roomType: '원룸',
     floor: '4층',
     size: '8평',
     kuanri: '5만',
     sogae: '최저가 가성비 탑',
+    detail:
+      '유동인구가 많은 곳이라 보안은 걱정 없으며 건물 내/외부에 CCTV가 설치 되어 있어요! 상수역 도보 4분거리에 위치해 있고~~',
   },
   {
     id: '2',
-    title: '신촌스테이하이',
+    title: '홍대 가성비 원룸',
     type: '월세',
-    money: 100,
-    bojoung: 100,
+    money: 50,
+    bojoung: 1000,
     roomType: '원룸',
-    floor: '4층',
-    size: '8평',
+    floor: '3층',
+    size: '5평',
     kuanri: '5만',
     sogae: '최저가 가성비 탑',
+    detail: '아이고 살기 조아라',
   },
   {
     id: '3',
@@ -78,7 +81,7 @@ const HomeListScreen = ({ navigation }) => {
           return (
             <HomeListButton
               title="list"
-              onPress={() => navigation.push('Map', item)}
+              onPress={() => navigation.navigate('ListInformation', item)}
             >
               <HomeListFrame
                 key={index}
@@ -106,7 +109,7 @@ const HomeListScreen = ({ navigation }) => {
 
 export default HomeListScreen;
 
-const Wrapper = styled.View`
+const Wrapper = styled.SafeAreaView`
   flex: 1;
 `;
 
