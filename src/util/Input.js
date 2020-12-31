95da67a444f1cb47479e378e99a6dfa0f6c1ae28
KIMJINOH97 <rlawlsdh1127@naver.com>
2020-12-31 16:38:23 +0900

    feat: Input style 생성

---
+++ src/util/Input.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const Input = ({}) => {
+  return <Wrapper></Wrapper>;
+};
+
+export default Input;
+
+export const InputStyle = styled.TextInput`
+  width: 100%;
+  height: 48px;
+  padding: 6px;
+  background-color: white;
+  border: 1px solid #dbdbdb;
+  border-radius: 5px;
+`;
