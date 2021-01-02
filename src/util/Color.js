b99aa495c25f3a2729663ec02d358b562d0b05e3
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-01-02 23:41:36 +0900

    feat: all of color in platform

---
+++ src/util/Color.js
@@ -0,0 +1,9 @@
+// 메인 색
+export const PRIMARY_NOMAL = '#FF766A';
+export const PRIMARY_LIGHT = '#FFB8B1';
+
+// 그레이
+export const DARK_GRAY = '#616161';
+export const MEDIUM_GRAY = '#9E9E9E';
+export const LIGHT_GRAY = '#BDBDBD';
+export const LIGHT_GRAY2 = '#EEEEEE';
