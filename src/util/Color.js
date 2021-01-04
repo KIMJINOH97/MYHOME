13fea8ec5992cc4f0170b6eba15ff4c1356c0e75
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-01-05 04:50:08 +0900

    chore: Add font

--- src/util/Color.js
+++ src/util/Color.js
@@ -1,9 +1,16 @@
 // 메인 색
-export const PRIMARY_NOMAL = '#FF766A';
+export const PRIMARY_NORMAL = '#FF766A';
 export const PRIMARY_LIGHT = '#FFB8B1';
-
+export const PRIMARY_LIGHT2 = 'rgba(255, 184, 177, 0.5)';
 // 그레이
 export const DARK_GRAY = '#616161';
 export const MEDIUM_GRAY = '#9E9E9E';
 export const LIGHT_GRAY = '#BDBDBD';
 export const LIGHT_GRAY2 = '#EEEEEE';
+
+export const NK100 = 'NotoSansKR_100Thin';
+export const NK300 = 'NotoSansKR_300Light';
+export const NK400 = 'NotoSansKR_400Regular';
+export const NK500 = 'NotoSansKR_500Medium';
+export const NK700 = 'NotoSansKR_700Bold';
+export const NK900 = 'NotoSansKR_900Black';
