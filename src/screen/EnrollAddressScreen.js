b03e5fb0c4344b5781d97f4b27e49eab1f3e9124
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-01-05 04:49:10 +0900

    chore: 주소찾기 눌렀을 시 전체화면 으로 수정

--- src/screen/EnrollAddressScreen.js
+++ src/screen/EnrollAddressScreen.js
@@ -1,47 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components/native';
-import { StatusBar } from 'react-native';
+import { Keyboard, StatusBar } from 'react-native';
 import Postcode from 'react-native-daum-postcode';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 import Title from '../util/Title';
 import InfoText from '../util/InfoText';
 import Input, { InputStyle } from '../util/Input';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
+import CompleteButton from '../util/CompleteButton';
 import { PRIMARY_NOMAL, MEDIUM_GRAY } from '../util/Color';
 
-const EnrollAddressScreen = () => {
+const EnrollAddressScreen = ({ user }) => {
+  const [addr, setAddr] = useState('주소지를 입력해주세요.');
+  const [isOpen, setIsOpen] = useState(false);
+  const [postNumber, setPostNumber] = useState('우편번호');
   return (
     <Wrapper>
       <Title name="주소 등록"></Title>
-      <KeyboardAwareScrollView>
-        <AddressContainer>
-          <InfoText name="우편번호" />
-          <PostNumberBox>
-            <TouchableOpacity
-              style={{ flexDirection: 'row', justifyContent: 'space-between' }}
-            >
-              <PostNumberView>
-                <PostNumberText>우편번호</PostNumberText>
-              </PostNumberView>
-              <PostNumberButtonView>
-                <PostNumberButtonText>우편번호 찾기</PostNumberButtonText>
-              </PostNumberButtonView>
-            </TouchableOpacity>
-          </PostNumberBox>
-          <InfoText name="주소지" />
-          <AddressView>
-            <AddressText>주소지를 입력해주세요.</AddressText>
-          </AddressView>
-          <InfoText name="상세주소" />
-          <DetailInput placeholder="상세주소를 입력해주세요." />
-        </AddressContainer>
-      </KeyboardAwareScrollView>
-      <Postcode
-        style={{ width: '100%', height: '100%' }}
-        jsOptions={{ animated: true }}
-        onSelected={(data) => alert(JSON.stringify(data))}
-      />
+      {isOpen ? (
+        <Postcode
+          style={{ width: '100%', height: '100%' }}
+          jsOptions={{ animated: true }}
+          onSelected={(data) => {
+            const {
+              userSelectedType,
+              roadAddress,
+              jibunAddress,
+              zonecode,
+            } = data;
+            if (userSelectedType === 'R') setAddr(roadAddress);
+            else setAddr(jibunAddress);
+            setIsOpen(!isOpen);
+            setPostNumber(zonecode);
+            console.log(jibunAddress, roadAddress);
+            return console.log(JSON.stringify(data));
+          }}
+        />
+      ) : (
+        <>
+          <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <AddressContainer>
+              <InfoText name="우편번호" />
+              <PostNumberBox>
+                <TouchableOpacity
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}
+                  onPress={() => setIsOpen(!isOpen)}
+                >
+                  <PostNumberView>
+                    <PostNumberText>{postNumber}</PostNumberText>
+                  </PostNumberView>
+                  <PostNumberButtonView>
+                    <PostNumberButtonText>우편번호 찾기</PostNumberButtonText>
+                  </PostNumberButtonView>
+                </TouchableOpacity>
+              </PostNumberBox>
+              <InfoText name="주소지" />
+              <AddressView>
+                <AddressText>{addr}</AddressText>
+              </AddressView>
+              <InfoText name="상세주소" />
+              <DetailInput placeholder="상세주소를 입력해주세요." />
+            </AddressContainer>
+          </TouchableWithoutFeedback>
+          <CompleteButton name="주소 등록 완료" />
+        </>
+      )}
     </Wrapper>
   );
 };
@@ -54,6 +81,7 @@ const Wrapper = styled.SafeAreaView`
 `;
 
 const AddressContainer = styled.View`
+  aspect-ratio: 0.8;
   padding-vertical: 10px;
   padding-horizontal: 15px;
 `;
@@ -93,7 +121,9 @@ const PostNumberButtonText = styled.Text`
 const AddressView = styled(PostNumberView)`
   width: 100%;
 `;
+
 const AddressText = styled.Text`
   color: ${MEDIUM_GRAY};
 `;
+
 const DetailInput = styled(InputStyle)``;
