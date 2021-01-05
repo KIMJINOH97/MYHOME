7c19f2cd85c9d98211ec8b1d70c19a9aedf67148
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-01-06 04:17:29 +0900

    chore: Fix homescreen component

--- src/components/Myhome/MyhomeSlogan.js
+++ src/components/Myhome/MyhomeSlogan.js
@@ -1,8 +1,8 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import styled from 'styled-components/native';
-
-import { PRIMARY_LIGHT2, DARK_GRAY } from '../../util/Color';
+import { StyleSheet, Text, View } from 'react-native';
+import { PRIMARY_LIGHT2, DARK_GRAY, NK500, NK700 } from '../../util/Color';
 
 const MyhomeSlogan = ({ navigation }) => {
   const nextPage = (page) => navigation.push(page);
@@ -10,16 +10,16 @@ const MyhomeSlogan = ({ navigation }) => {
     <Wrapper>
       <SloganView>
         <SloganContentView>
-          <SloganContent>
+          <SloganContentFirst>
             멘토와 함께라면{'\n'}직거래도 무섭지 않아
-          </SloganContent>
-          <SubContent>
+          </SloganContentFirst>
+          <SloganContentSecond>
             매물 찾기부터 계약까지{'\n'}A to Z 도우미 서비스.
-          </SubContent>
+          </SloganContentSecond>
         </SloganContentView>
         <MentoApplyButtonView>
           <MentoApplyButton onPress={() => nextPage('ApplyMento')}>
-            <MentoAppleyButtonContent>멘토신청하기</MentoAppleyButtonContent>
+            <MentoApplyButtonContent>멘토신청하기</MentoApplyButtonContent>
           </MentoApplyButton>
         </MentoApplyButtonView>
       </SloganView>
@@ -29,36 +29,43 @@ const MyhomeSlogan = ({ navigation }) => {
 
 export default MyhomeSlogan;
 
+const SloganContentFirst = styled.Text`
+  font-family: ${NK700};
+  font-size: 22px;
+  letter-spacing: -1.1px;
+  margin-bottom: -14px;
+`;
+
+const SloganContentSecond = styled.Text`
+  font-family: ${NK700};
+  font-size: 15px;
+  letter-spacing: -0.17px;
+  color: ${DARK_GRAY};
+`;
+
+const MentoApplyButtonContent = styled.Text`
+  font-family: ${NK700};
+  font-size: 16px;
+`;
+
 const Wrapper = styled.View`
   flex: 5;
   /* background-color: red; */
 `;
 
 const SloganView = styled.View`
+  display: flex;
   height: 240px;
   padding-horizontal: 16px;
   background-color: ${PRIMARY_LIGHT2};
   border-radius: 6px;
-  padding-top: 27px;
+  padding-top: 8px;
   padding-bottom: 21px;
   justify-content: space-between;
   margin-bottom: 16px;
 `;
 
 const SloganContentView = styled.View``;
-
-const SloganContent = styled.Text`
-  font-size: 22px;
-  font-weight: 700;
-  letter-spacing: -1.1px;
-  margin-bottom: 20px;
-`;
-
-const SubContent = styled.Text`
-  color: ${DARK_GRAY};
-  font-size: 16px;
-  font-weight: 700;
-`;
 
 const MentoApplyButtonView = styled.View`
   height: 40px;
@@ -71,9 +78,4 @@ const MentoApplyButton = styled.TouchableOpacity`
   justify-content: center;
   align-items: center;
   border-radius: 8px;
-`;
-
-const MentoAppleyButtonContent = styled.Text`
-  font-size: 16px;
-  font-weight: 700;
 `;
