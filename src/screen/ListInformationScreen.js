296ee85eb5573c60843cbccfa786165bfb5dbd59
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-01-06 22:56:47 +0900

    feat: platform icon

--- src/screen/ListInformationScreen.js
+++ src/screen/ListInformationScreen.js
@@ -1,15 +1,38 @@
-import React from 'react';
-import { Text, View, SafeAreaView, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  Dimensions,
+  Image,
+} from 'react-native';
 import styled from 'styled-components/native';
 import Swiper from 'react-native-swiper';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 
 import DetailContent from '../components/ListInfromation/DetailContent';
-import { useNavigation } from '@react-navigation/native';
+import SimpleList from '../components/ListInfromation/SimpleList';
+import DivideLine from '../util/DivideLine';
+import { TextStyle } from '../util/TextStyle';
+import {
+  DARK_GRAY,
+  LIGHT_GRAY,
+  LIGHT_GRAY2,
+  MEDIUM_GRAY,
+  NK500,
+  NK700,
+  PRIMARY_LIGHT2,
+  PRIMARY_NORMAL,
+} from '../util/Color';
+import LIST_HOME from '../../assets/LIST_HOME.png';
+import LIST_MANAGE from '../../assets/LIST_MANAGE.png';
+import LIST_SIZE from '../../assets/LIST_SIZE.png';
+import LIST_FLOOR from '../../assets/LIST_FLOOR.png';
+import HOME_PICTURE from '../../assets/HOME_PICTURE.png';
 
-const ListInformationScreen = ({ route }) => {
+const FirstRoute = ({ information }) => {
   const {
-    title,
-    type,
     money,
     bojoung,
     roomType,
@@ -18,13 +41,90 @@ const ListInformationScreen = ({ route }) => {
     kuanri,
     sogae,
     detail,
+  } = information;
+  return (
+    <>
+      <DetailInformation>
+        <DetailTitle>
+          <DetailTitleContent>상세정보</DetailTitleContent>
+        </DetailTitle>
+        <DivideLine height="1px" color={LIGHT_GRAY2} />
+        <DetailContent name={'방종류'} info={roomType} />
+        <DetailContent name={'가격'} info={`${bojoung}/${money}`} />
+        <DetailContent name={'관리비'} info={kuanri} />
+        <DetailContent name={'층수'} info={floor} />
+        <DetailContent name={'실평수'} info={size} />
+        <DetailContent name={'준공년도'} info={roomType} />
+        <DetailContent
+          alignItem="flex-start"
+          name={'추가옵션'}
+          info={roomType}
+        />
+        <DetailContent name={'보일러'} info={roomType} />
+        <DetailContent name={'입주가능일'} info={roomType} />
+        <DetailContent alignItem="flex-start" name={'상세설명'} info={detail} />
+      </DetailInformation>
+    </>
+  );
+};
+
+const SecondRoute = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>하하하하하하하</Text>
+    </View>
+  );
+};
+
+const ThirdRoute = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>하하하하하하하</Text>
+    </View>
+  );
+};
+
+const initialLayout = { width: Dimensions.get('window').width };
+
+const ListInformationScreen = ({ route }) => {
+  const {
+    title,
+    money,
+    type,
+    roomType,
+    size,
+    floor,
+    kuanri,
+    bojoung,
   } = route.params;
   // const home = useNavigation().setParams('item');
+
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    {
+      key: 'first',
+      title: '정보',
+    },
+    {
+      key: 'second',
+      title: '리뷰',
+    },
+    {
+      key: 'third',
+      title: '집주인 정보',
+    },
+  ]);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ScrollView>
-        <Swiper height={220}>
-          <Red />
+        <Swiper height={240}>
+          <Red>
+            <Image
+              source={HOME_PICTURE}
+              style={{ width: '100%', height: '100%' }}
+            />
+          </Red>
           <Yellow />
           <Blue />
         </Swiper>
@@ -36,25 +136,40 @@ const ListInformationScreen = ({ route }) => {
             <SubContent>{title}</SubContent>
           </ListTitle>
           <SimpleInformation>
-            <SimpleContent>{roomType}</SimpleContent>
-            <SimpleContent>{size}</SimpleContent>
-            <SimpleContent>{floor}</SimpleContent>
-            <SimpleContent>{kuanri}</SimpleContent>
+            <SimpleList img={LIST_HOME} info={roomType} />
+            <SimpleList img={LIST_SIZE} info={size} />
+            <SimpleList img={LIST_FLOOR} info={floor} />
+            <SimpleList img={LIST_MANAGE} info={kuanri} />
           </SimpleInformation>
-          <DetailInformation>
-            <DetailTitle>상세정보</DetailTitle>
-          </DetailInformation>
-          <DevideLine />
-          <DetailContent name={'방종류'} info={roomType} />
-          <DetailContent name={'가격'} info={`${bojoung}/${money}`} />
-          <DetailContent name={'관리비'} info={kuanri} />
-          <DetailContent name={'층수'} info={floor} />
-          <DetailContent name={'실평수'} info={size} />
-          <DetailContent name={'준공년도'} info={roomType} />
-          <DetailContent name={'추가옵션'} info={roomType} />
-          <DetailContent name={'보일러'} info={roomType} />
-          <DetailContent name={'입주가능일'} info={roomType} />
-          <DetailContent name={'상세설명'} info={detail} />
+          <TabView
+            renderTabBar={(props) => (
+              <TabBar
+                {...props}
+                indicatorStyle={{
+                  backgroundColor: PRIMARY_NORMAL,
+                }}
+                style={{
+                  backgroundColor: '#ffeae8',
+                }}
+                labelStyle={{
+                  alignItems: 'center',
+                  fontFamily: NK700,
+                  fontSize: 14,
+                  includeFontPadding: false,
+                }}
+                activeColor={PRIMARY_NORMAL}
+                inactiveColor={MEDIUM_GRAY}
+              />
+            )}
+            navigationState={{ index, routes }}
+            renderScene={SceneMap({
+              first: () => <FirstRoute information={route.params} />,
+              second: () => <SecondRoute information={route.params} />,
+              third: () => <ThirdRoute information={route.params} />,
+            })}
+            onIndexChange={setIndex}
+            initialLayout={initialLayout}
+          />
         </InfromationContainer>
       </ScrollView>
     </SafeAreaView>
@@ -63,18 +178,16 @@ const ListInformationScreen = ({ route }) => {
 
 export default ListInformationScreen;
 
-const HomePicture = styled.View`
-  flex: 1;
-`;
-
 const Red = styled.View`
   flex: 1;
   background-color: red;
 `;
+
 const Yellow = styled.View`
   flex: 1;
   background-color: yellow;
 `;
+
 const Blue = styled.View`
   flex: 1;
   background-color: blue;
@@ -82,46 +195,49 @@ const Blue = styled.View`
 
 const InfromationContainer = styled.View`
   flex: 1;
-  padding: 10px;
 `;
 
 const ListTitle = styled.View`
   justify-content: center;
   align-items: center;
 `;
-const TitleContent = styled.Text`
-  font-size: 20px;
-  font-weight: 700;
-  letter-spacing: -1px;
+
+const TitleContent = styled(TextStyle)`
+  font-family: ${NK700};
+  font-size: 24px;
+  letter-spacing: -1.2px;
 `;
-const SubContent = styled.Text`
-  font-size: 15px;
-  font-weight: 700;
-  color: #616161;
+
+const SubContent = styled(TextStyle)`
+  font-family: ${NK500};
+  font-size: 16px;
+  color: ${MEDIUM_GRAY};
 `;
 
 const SimpleInformation = styled.View`
   flex-direction: row;
   justify-content: space-around;
   margin-top: 20px;
-`;
-const SimpleContent = styled.Text`
-  color: #9e9e9e;
-  font-weight: 700;
+  border-top-color: ${LIGHT_GRAY2};
+  border-top-width: 1px;
+  padding: 10px;
 `;
 
 const DetailInformation = styled.View`
   margin-top: 20px;
+  padding-horizontal: 16px;
 `;
 
-const DetailTitle = styled.Text`
-  font-size: 15px;
-  font-weight: 700;
+const DetailTitle = styled.View``;
+
+const DetailTitleContent = styled(TextStyle)`
+  font-family: ${NK500};
+  font-size: 16px;
 `;
 
 const DevideLine = styled.View`
   height: 1px;
-  background-color: #9e9e9e;
+  background-color: ${DARK_GRAY};
   margin-bottom: 10px;
   margin-top: 10px;
 `;
