f789cf6bcab538881dd3ee3146972b583bd9608a
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-01-12 03:58:05 +0900

    chore: 관심매물 페이지 수정

--- src/screen/FavoriteScreen.js
+++ src/screen/FavoriteScreen.js
@@ -3,12 +3,32 @@ import styled from 'styled-components/native';
 import { StatusBar, Text, Platform } from 'react-native';
 
 import TabTitle from '../util/TabTitle';
+import { TextStyle } from '../util/TextStyle';
+import { useRecoilState } from 'recoil';
+import { userState } from '../states/LoginState';
+import { LIGHT_GRAY, NK500 } from '../util/Color';
+
+import PLEASE_LOGIN from '../../assets/PLEASE_LOGIN.png';
 
 const FavoriteScreen = () => {
+  const [user] = useRecoilState(userState);
   return (
     <Wrapper>
       <TabTitle name="관심매물" />
-      <FavoriteContainer></FavoriteContainer>
+      <FavoriteContainer>
+        {user ? (
+          <></>
+        ) : (
+          <>
+            <PleaseLoginView>
+              <LoginContent>로그인이 필요한 서비스입니다.</LoginContent>
+            </PleaseLoginView>
+            <HomiImageView>
+              <HomiImage source={PLEASE_LOGIN} />
+            </HomiImageView>
+          </>
+        )}
+      </FavoriteContainer>
     </Wrapper>
   );
 };
@@ -23,4 +43,26 @@ const Wrapper = styled.SafeAreaView`
 
 const FavoriteContainer = styled.View`
   flex: 11;
+`;
+
+const PleaseLoginView = styled.View`
+  flex: 2;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoginContent = styled(TextStyle)`
+  font-family: ${NK500};
+  font-size: 20px;
+  color: ${LIGHT_GRAY};
+`;
+
+const HomiImageView = styled.View`
+  flex: 8;
+  align-items: center;
+`;
+
+const HomiImage = styled.Image`
+  height: 303px;
+  width: 218px;
 `;
