36279a8a90c03ed22a6d5464ebdca434d9bceb31
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-01-25 19:30:27 +0900

    feat: airbnb eslint 적용

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/react',
+    'plugin:prettier/recommended',
+    'eslint-config-prettier',
+  ],
+  parser: 'babel-eslint',
+  rules: {
+    'import/no-unresolved': 'off',
+    camelcase: 'off',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+        printWidth: 100,
+      },
+    ],
+  },
+  plugins: ['prettier'],
+};
